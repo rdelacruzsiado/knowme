12ebc46a0d13e8702dd4decd4f6e72358b283d3e
Robinson De La Cruz <rdelacruzsiado@gmail.com>
2022-03-26 22:17:12 -0500

    feat: Add middleware to handle the errors

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const routerApi = require("./routes");
 const config = require("./config");
+const {
+  logErrors,
+  boomErrorHandler,
+  errorHandler,
+} = require("./middlewares/error.handler");
 
 const app = express();
 
 routerApi(app);
+
+app.use(logErrors);
+app.use(boomErrorHandler);
+app.use(errorHandler);
 
 app.listen(config.port, () => {
   console.log(`Listening on port ${config.port}`);
